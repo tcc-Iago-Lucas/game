b5863dc158480723cdb9b671b0a6588ab2461335
Lucas Rocha <lucas.freitas@sigcertificadora.com.br>
2020-08-05 19:54:25 -0300

    mostrando o retorno na tela do jogo

--- js/plugins/api.js
+++ js/plugins/api.js
@@ -4,28 +4,52 @@ ajax.setRequestHeader("Content-type", "application/json;charset=UTF-8");
 ajax.send(JSON.stringify({ "description": "testando exeternal file" }));
  */
 
- api =  {
-    
-     post(json) {
-        var ajax = new XMLHttpRequest();
+
+api = {
+    getAjax (){
+        var ajax;
+        if(ajax == null){
+           ajax = new XMLHttpRequest();
+        }
+        return ajax;
+    }, 
+    post(json) {
+        var ajax = this.getAjax();
         ajax.open("POST", "http://localhost:5759/role/create", true);
         ajax.setRequestHeader("Content-type", "application/json;charset=UTF-8");
-        ajax.onreadystatechange = function () {
+        ajax.onreadystatechange = async function () {
 
             if (ajax.readyState == 4 && ajax.status == 200) {
 
-                var {id} = JSON.parse(ajax.response);
+                var { id } = JSON.parse(ajax.response);
                 // Retorno do Ajax
                 $gameVariables._data[4] = id;
-                console.log("response da api, id = " , $gameVariables._data[4]);
-                
+                console.log("response da api, id = ", $gameVariables._data[4]);
+
             }
-            
+
         }
         ajax.send(JSON.stringify(json));
-        
+
     },
-    teste(){
-        return 99;
+    get(){
+        console.log("veio no get api");
+        var ajax = this.getAjax();
+        ajax.open("GET", "http://localhost:5759/role", true);
+        ajax.setRequestHeader("Content-type", "application/json;charset=UTF-8");
+        ajax.send();
+        ajax.onreadystatechange = function () {
+
+            // Caso o state seja 4 e o http.status for 200, é porque a requisiçõe deu certo.
+            if (ajax.readyState == 4 && ajax.status == 200) {
+
+                var data = JSON.parse(ajax.response);
+                // Retorno do Ajax
+                console.log(data);
+            }
+
+        }
+
     }
+    
 }
