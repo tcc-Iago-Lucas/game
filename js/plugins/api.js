714c28d27dd41718be7b0a07d4eaf4b25982e4e0
Lucas Rocha <lucas.freitas@sigcertificadora.com.br>
2020-08-04 21:17:52 -0300

    conseguindo vizualizar a resposta do banco no console.log

--- js/plugins/api.js
+++ js/plugins/api.js
@@ -1,4 +1,31 @@
-var ajax = new XMLHttpRequest();
+/* var ajax = new XMLHttpRequest();
 ajax.open("POST", "http://localhost:5759/role/create", true);
 ajax.setRequestHeader("Content-type", "application/json;charset=UTF-8");
-ajax.send(JSON.stringify({ "description": "testando exeternal file"}));
+ajax.send(JSON.stringify({ "description": "testando exeternal file" }));
+ */
+
+ api =  {
+    
+     post(json) {
+        var ajax = new XMLHttpRequest();
+        ajax.open("POST", "http://localhost:5759/role/create", true);
+        ajax.setRequestHeader("Content-type", "application/json;charset=UTF-8");
+        ajax.onreadystatechange = function () {
+
+            if (ajax.readyState == 4 && ajax.status == 200) {
+
+                var {id} = JSON.parse(ajax.response);
+                // Retorno do Ajax
+                $gameVariables._data[4] = id;
+                console.log("response da api, id = " , $gameVariables._data[4]);
+                
+            }
+            
+        }
+        ajax.send(JSON.stringify(json));
+        
+    },
+    teste(){
+        return 99;
+    }
+}
